4e23b23534f3d08b273ebf1f17829e6278b930dc
1000len-8054 <alexhorst58@gmail.com>
2025-10-14 12:37:51 +0200

    Project Initialisation

---
+++ README.md
@@ -0,0 +1,16 @@
+# 🎮 Siedler Frontend
+
+This is the frontend for our game **Siedler**, an alternative to Settlers of Catan.
+
+Players are supposed to access this via the browser. This code is not meant for installation, rather just for development and deployment.
+
+## 🛠️ Development
+
+To start the dev server:
+
+```bash
+git clone git@github.com:Hexfeld-Studio/Siedler-Frontend.git
+cd Siedler-Frontend
+bun install
+bun run dev
+```
---
+++ eslint.config.js
@@ -0,0 +1,53 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
+import reactX from 'eslint-plugin-react-x'
+import reactDom from 'eslint-plugin-react-dom'
+
+/*
+DEFAULT CONFIG
+
+*/
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+
+      // RECOMMENDATION FROM THE DEFAULT README FOR PRODUCTION ENVIRONMENTS
+      
+      // # Type Awareness
+      // Remove tseslint.configs.recommended and replace with this
+      tseslint.configs.recommendedTypeChecked,
+      // Alternatively, use this for stricter rules
+      tseslint.configs.strictTypeChecked,
+      // Optionally, add this for stylistic rules
+      tseslint.configs.stylisticTypeChecked,
+
+      // # React Specific
+      // Enable lint rules for React
+      reactX.configs['recommended-typescript'],
+      // Enable lint rules for React DOM
+      reactDom.configs.recommended,
+
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+
+      // RECOMMENDATION FROM THE DEFAULT README FOR PRODUCTION ENVIRONMENTS
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+])
+
