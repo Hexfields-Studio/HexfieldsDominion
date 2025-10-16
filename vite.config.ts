08e3a0c7cefe1571bb5c388b27b72538dd90e50b
Marcel Steinle <marcelsteinle05@gmail.com>
2025-10-16 14:09:47 +0200

    Added CI/CD Pioeline (.ts type checking and GitHub Pages deployment)

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // added for CI/CD. Will put the game under https://hexfeldstudio.githib.io/siedler-frontend.
 })
