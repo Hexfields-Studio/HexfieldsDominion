f8c363331f9a684e8fa738d09695bf587ebd5a83
Marcel Steinle <marcelsteinle05@gmail.com>
2025-10-16 14:27:08 +0200

    Trying again

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // added for CI/CD. Will put the game under https://hexfeldstudio.githib.io/siedler-frontend.
+  base: '/Siedler-Frontend/', // added for CI/CD. Will put the game and assets under https://hexfeldstudio.githib.io/Siedler-Frontend/.
 })
