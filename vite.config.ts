87a669670775017ddf62b5a2781695ef5ea12edd
Marcel Steinle <marcelsteinle05@gmail.com>
2025-10-18 12:58:29 +0200

    UPDATE base path because of name change

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Siedler-Frontend/', // added for CI/CD. Will put the game and assets under https://hexfeldstudio.githib.io/Siedler-Frontend/.
+  base: '/HexfieldsDominion/', // added for CI/CD. Will put the game and assets under https://Hexfields-Studio.githib.io/HexfieldsDominion/.
 })
