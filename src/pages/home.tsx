0aa04527b48b56c30b974a64588a328410e433de
Alexander Horst <118634038+A1exHorst@users.noreply.github.com>
2025-11-05 10:51:52 +0100

    Demo Finetuning + Attempt to fix a bug (#7)
    * Routing works, ContextTest not yet finished
    
    * UPDATE: Implemented a localStorage because ContextVariables get resetted on a remount (reloading the page or directly entering a suburl)
    
    * comment
    
    * Demo finetuning + Attempt to fix a bug on github Pages where the router does not find the suburls

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
 import "../index.css";
 import { getStorageItem, setStorageItem, STORAGE_KEYS } from "../constants/storage";
+import { useNavigate } from "react-router";
 
 
 const HomePage = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(getStorageItem(STORAGE_KEYS.IS_LOGGED_IN, false));
+  const [codeOrUUID, setCodeOrUUID] = useState("");
+  const navi = useNavigate();
 
   useEffect(() => {
     setStorageItem(STORAGE_KEYS.IS_LOGGED_IN, isLoggedIn);
@@ -14,7 +17,10 @@ const HomePage = () => {
       <>
         <h1>This is the HomePage</h1>
         <p>Logged In: {isLoggedIn ? "true" : "false"}</p>
-        <button onClick={()=>setIsLoggedIn(!isLoggedIn)}>Toggle "isLoggedIn"</button>
+        <button onClick={()=>setIsLoggedIn(!isLoggedIn)}>Toggle "isLoggedIn"</button><br/>
+        <input type="text" onChange={(e) => {setCodeOrUUID(e.target.value)}} placeholder="code/uuid"/>
+        <button onClick={()=>{navi(`/lobby/${codeOrUUID}`);}}>Join Lobby</button>
+        <button onClick={()=>{navi(`/match/${codeOrUUID}`);}}>Join Match</button>
       </>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/HexfieldsDominion/', // added for CI/CD. Will put the game and assets under https://Hexfields-Studio.githib.io/HexfieldsDominion/.
+  base: '/HexfieldsDominion/', // added for CI/CD. Will put the game and assets under https://Hexfields-Studio.github.io/HexfieldsDominion/.
 })
