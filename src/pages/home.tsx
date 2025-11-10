7ecd9f6a8a0594d116dc6d1c95f922133766fa71
Marcel Steinle <marcelsteinle05@gmail.com>
2025-11-10 16:45:36 +0100

    Demo 1 patch: Added lobby requesting and environment variables

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -6,7 +6,6 @@ import { useNavigate } from "react-router";
 
 const HomePage = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(getStorageItem(STORAGE_KEYS.IS_LOGGED_IN, false));
-  const [codeOrUUID, setCodeOrUUID] = useState("");
   const navi = useNavigate();
 
   useEffect(() => {
@@ -15,12 +14,22 @@ const HomePage = () => {
 
   return (
       <>
-        <h1>This is the HomePage</h1>
-        <p>Logged In: {isLoggedIn ? "true" : "false"}</p>
-        <button onClick={()=>setIsLoggedIn(!isLoggedIn)}>Toggle "isLoggedIn"</button><br/>
-        <input type="text" onChange={(e) => {setCodeOrUUID(e.target.value)}} placeholder="code/uuid"/>
-        <button onClick={()=>{navi(`/lobby/${codeOrUUID}`);}}>Join Lobby</button>
-        <button onClick={()=>{navi(`/match/${codeOrUUID}`);}}>Join Match</button>
+        <h1>Home Page</h1>
+        <p>
+          Logged In: {isLoggedIn ? "true" : "false"} <br/>
+          <button onClick={()=>setIsLoggedIn(!isLoggedIn)}>Toggle "isLoggedIn"</button>
+        </p>
+        <p>
+          <button onClick={() => {
+            // Nur wenn eingeloggt weiterleiten
+            if (isLoggedIn) {
+              navi('/play');
+            } else {
+              // Muss noch mit eigenem Popup ersetzt werden
+              alert("You must be logged in to proceed to Play.");
+            }
+          }}>Login</button>
+        </p>
       </>
   );
 }
