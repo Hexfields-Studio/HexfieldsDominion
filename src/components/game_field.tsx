62973aaca46f577ee836cb4454c468aaf5cfa138
1000len-8054 <alexhorst58@gmail.com>
2025-11-13 13:53:48 +0100

    Fixed a bug when resizing the window Resizing the window (Browser) now makes sure that the center of the gamefield does not exit the screen.

--- src/components/game_field.tsx
+++ src/components/game_field.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from "react";
-import { Layer, Rect, RegularPolygon, Stage } from "react-konva";
+import { Layer, RegularPolygon, Stage } from "react-konva";
 import "./game_field.css";
 import type Konva from "konva";
 
@@ -13,28 +13,54 @@ interface GameFieldProps {
     boardRadius: number
 }
 
+const radius: number = 100;
+
+function generateHexagons(newHexagons: hexagon[], boardRadius: number) {
+    console.log("Generating")
+    for (let q = -boardRadius + 1; q <= boardRadius - 1; q++) {
+        const r1 = Math.max(-boardRadius + 1, -q - boardRadius + 1);
+        const r2 = Math.min(boardRadius - 1, -q + boardRadius - 1);
+        for (let r = r1; r <= r2; r++) {
+            const x = (q + r/2) * Math.sqrt(3) * radius;
+            const y = r * (3/2) * radius;
+            newHexagons.push({x, y, fill: "gold"});
+        }
+    }
+}
+
 const GameField: React.FC<GameFieldProps> = ({boardRadius}) => {
-    const hexagons: hexagon[] = [];
-    const [radius, setRadius] = useState(100);
+    const [hexagons, setHexagons] = useState<hexagon[]>([]);
 
     const containerRef = useRef<HTMLDivElement>(null);
     const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
 
     //For Camera Dragging
     const [cameraOffset, setCameraOffset] = useState({ x: 0, y: 0 });
+    const cameraOffsetRef = useRef(cameraOffset);
     const [isDragging, setIsDragging] = useState(false);
-    const [dragStart, setDragStart] = useState({ x_change: 0, y_change: 0 });
+    const [dragStart, setDragStart] = useState({ x_mouse: 0, y_mouse: 0 });
     const [containerClientSize, setContainerClientSize] = useState({width: 0, height: 0});
 
-    for (let q = -boardRadius + 1; q <= boardRadius - 1; q++) {
-        const r1 = Math.max(-boardRadius + 1, -q - boardRadius + 1);
-        const r2 = Math.min(boardRadius - 1, -q + boardRadius - 1);
-        for (let r = r1; r <= r2; r++) {
-            const x = (q + r/2) * Math.sqrt(3) * radius + window.innerWidth/2;
-            const y = r * (3/2) * radius + window.innerHeight/2;
-            hexagons.push({x, y, fill: "lightblue"});
+    useEffect(() => {
+        cameraOffsetRef.current = cameraOffset;
+    }, [cameraOffset]);
+
+    const handleResize = (container: HTMLDivElement | null) => {
+        if (container) {
+            setDimensions({width: container.offsetWidth, height: container.offsetHeight});
+
+            const clientWidth: number = container.clientWidth;
+            const clientHeight: number = container.clientHeight;
+            setContainerClientSize({width: clientWidth, height: clientHeight});
+
+            const halfWidth = clientWidth / 2;
+            const halfHeight = clientHeight / 2;
+            setCameraOffset({
+                x: Math.max(-halfWidth, Math.min(cameraOffsetRef.current.x, halfWidth)),
+                y: Math.max(-halfHeight, Math.min(cameraOffsetRef.current.y, halfHeight)),
+            });
         }
-    }
+    };
 
     useEffect(() => {
         const container: HTMLDivElement | null = containerRef.current;
@@ -46,62 +72,48 @@ const GameField: React.FC<GameFieldProps> = ({boardRadius}) => {
             });
         }
 
-        const handleResize = () => {
-            if (container) {
-                setDimensions({width: container.offsetWidth, height: container.offsetHeight});
-                setContainerClientSize({width: container.clientWidth, height: container.clientHeight});
-                moveCamera(dragStart);
-            }
-        };
+        const newHexagons: hexagon[] = []
+        generateHexagons(newHexagons, boardRadius)
+        setHexagons(newHexagons);
 
-        window.addEventListener("resize", handleResize);
-        return () => window.removeEventListener("resize", handleResize);
+        window.addEventListener("resize", ()=>handleResize(container));
+        return () => window.removeEventListener("resize", ()=>handleResize(container));
     }, []);
 
-    const handleMouseDown = (e: Konva.KonvaEventObject<MouseEvent>) => {
-        console.log(cameraOffset, containerRef.current?.clientWidth, containerRef.current?.clientHeight);
-        setIsDragging(true);
-        setDragStart({
-            x_change: e.evt.clientX - cameraOffset.x,
-            y_change: e.evt.clientY - cameraOffset.y,
-        });
-    };
-
-    const handleMouseMove = (e: Konva.KonvaEventObject<MouseEvent>) => {
-        if (!isDragging) return;
-        moveCamera({x_change: e.evt.clientX, y_change: e.evt.clientY});
-    };
-
-    function moveCamera({x_change, y_change}: {x_change: number, y_change: number}) {
+    function moveCamera({x_mouse, y_mouse}: {x_mouse: number, y_mouse: number}) {
         const halfWidth = containerClientSize.width / 2;
         const halfHeight = containerClientSize.height / 2;
 
-        let x: number = x_change - dragStart.x_change;
-        x = Math.max(-halfWidth, Math.min(x, halfWidth));
-        let y: number =  y_change - dragStart.y_change;
-        y = Math.max(-halfHeight, Math.min(y, halfHeight));
+        let newCameraOffsetX: number = x_mouse - dragStart.x_mouse;
+        newCameraOffsetX = Math.max(-halfWidth, Math.min(newCameraOffsetX, halfWidth));
+        let newCameraOffsetY: number =  y_mouse - dragStart.y_mouse;
+        newCameraOffsetY = Math.max(-halfHeight, Math.min(newCameraOffsetY, halfHeight));
         setCameraOffset({
-            x: x,
-            y: y,
+            x: newCameraOffsetX,
+            y: newCameraOffsetY,
         });
     }
-
-
-    const handleMouseUp = () => {
-        setIsDragging(false);
-    };
 
     return (
         <div ref={containerRef} className="full-page-container">
             <Stage
                 width={dimensions.width}
                 height={dimensions.height}
-                onMouseDown={handleMouseDown}
-                onMouseMove={handleMouseMove}
-                onMouseUp={handleMouseUp}
-                onMouseLeave={handleMouseUp}
+                onMouseDown={(e: Konva.KonvaEventObject<MouseEvent>) => {
+                    setIsDragging(true);
+                    setDragStart({
+                        x_mouse: e.evt.clientX - cameraOffset.x,
+                        y_mouse: e.evt.clientY - cameraOffset.y,
+                    });
+                }}
+                onMouseMove={(e: Konva.KonvaEventObject<MouseEvent>) => {
+                    if (!isDragging) return;
+                    moveCamera({x_mouse: e.evt.clientX, y_mouse: e.evt.clientY});
+                }}
+                onMouseUp={()=>setIsDragging(false)}
+                onMouseLeave={()=>setIsDragging(false)}
             >
-                <Layer x={cameraOffset.x} y={cameraOffset.y}>
+                <Layer x={cameraOffset.x + dimensions.width / 2} y={cameraOffset.y + dimensions.height / 2}>
                     {hexagons.map((hex, i) => (
                     <RegularPolygon
                         key={i}
