4e2b0203fcce71440b27eb207d510703ef23fa2f
Jona <62996543+JaskerX@users.noreply.github.com>
2026-05-21 16:03:29 +0200

    change lobby page based on isOwner (#77)

--- src/constants/selectStyles.ts
+++ src/constants/selectStyles.ts
@@ -1,19 +1,20 @@
-import type { CSSObjectWithLabel, StylesConfig } from "react-select";
+import type { ControlProps, CSSObjectWithLabel, DropdownIndicatorProps, GroupBase, IndicatorSeparatorProps, StylesConfig } from "react-select";
 import type { SelectOption } from "./customTypes";
 
 export const DefaultSelectStyle: StylesConfig<SelectOption, false> = ({
-  control: (base: CSSObjectWithLabel) => ({
+  control: (base: CSSObjectWithLabel, props: ControlProps<SelectOption, false, GroupBase<SelectOption>>) => ({
     ...base,
     cursor: "pointer",
-    backgroundColor: "var(--input-background)",
-    border: "2px solid var(--input-border)",
+    backgroundColor: props.isDisabled ? "color-mix(in srgb, #000000 40%, var(--input-background) 60%)" : "var(--input-background)",
+    border: props.isDisabled ? "2px solid color-mix(in srgb, #000000 40%, var(--input-border) 60%)" : "2px solid var(--input-border)",
     "&:hover": {
       borderColor: "color-mix(in srgb, var(--foreground) 20%, var(--input-border) 80%)",
     },
     boxShadow: "none",
   }),
-  dropdownIndicator: (base: CSSObjectWithLabel) => ({
+  dropdownIndicator: (base: CSSObjectWithLabel, props: DropdownIndicatorProps<SelectOption, false, GroupBase<SelectOption>>) => ({
     ...base,
+    opacity: props.isDisabled ? 0 : 1,
     "&:hover": {
       color: "lightgray",
     },
@@ -39,5 +40,9 @@ export const DefaultSelectStyle: StylesConfig<SelectOption, false> = ({
     "&:hover": {
       backgroundColor: "color-mix(in srgb, var(--foreground) 5%, var(--input-background) 95%)",
     },
+  }),
+  indicatorSeparator: (base: CSSObjectWithLabel, props: IndicatorSeparatorProps<SelectOption, false, GroupBase<SelectOption>>) => ({
+    ...base,
+    opacity: props.isDisabled ? 0 : 1,
   }),
 });
