5800860d6a3fe6a5003aaf55e06557cb913db2f3
Marcel Steinle <marcelsteinle05@gmail.com>
2026-06-02 12:32:19 +0200

    Typo in import

--- src/components/gameField/gameGui/buildPanel/buildPanel.tsx
+++ src/components/gameField/gameGui/buildPanel/buildPanel.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./BuildPanel.module.scss";
+import styles from "./buildPanel.module.scss";
 
 export type BuildType = "SETTLEMENT" | "STREET" | "TOWN" | null;
 
